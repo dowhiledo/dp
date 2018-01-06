64fe4f91962268882978a94c78a5291bf3d4755e
userk <userk@email.com>
2018-01-06 14:57:27 -0500

    <td>img<td>

--- musculos/index.php
+++ musculos/index.php
@@ -9,21 +9,24 @@ $ruta = "../imagenes/";
 $nombre = "cerdo.jpg";
 
 
-function crearImg($ruta,$nombre, $clase, $dim, $puntos) {
-    $src = $ruta."/".$nombre;
+function crearImg($ruta, $nombre, $clase, $dim, $puntos) {
+    $src = $ruta . "/" . $nombre;
     $tagImg = "<img src=$src class=$clase width=$dim height=$dim data-puntos=$puntos>";
     return $tagImg;
 }
 
-$f1a = crearImg($ruta,"cerdo.jpg" ,"fila1", 50, 1);
-$f1b = crearImg($ruta,"tigre.jpg" ,"fila1", 50, 2);
-$f1c = crearImg($ruta,  "ojo.jpg" ,"fila1", 50, 3);
+$f11 = crearImg($ruta, "cerdo.jpg", "fila1", 50, 1);
+$f12 = crearImg($ruta, "tigre.jpg", "fila1", 50, 2);
+$f13 = crearImg($ruta, "ojo.jpg", "fila1", 50, 3);
 
+$f21 = crearImg($ruta, "cerdo1.jpg", "fila2", 50, 1);
+$f22 = crearImg($ruta, "tigre1.jpg", "fila2", 50, 2);
+$f23 = crearImg($ruta, "ojo1.jpg", "fila2", 50, 3);
 
 ?>
 
 
-<table class="table">
+<table class="table tabla1mus">
     <thead>
     <tr>
         <th>Row</th>
@@ -35,33 +38,17 @@ $f1c = crearImg($ruta,  "ojo.jpg" ,"fila1", 50, 3);
     <tbody>
     <tr>
         <td>musculos</td>
-        <td>
-            <?php echo $f1a ?>
-        </td>
-        <td>
-            <?php echo $f1b; ?>
-        </td>
-        <td>
-            <?php echo $f1c; ?>
-        </td>
-        <td>
-            <label for="" id="ptsFila1">0</label>
-        </td>
+        <td><?php echo $f11 ?></td>
+        <td><?php echo $f12 ?></td>
+        <td><?php echo $f13 ?></td>
+        <td><label for="" id="ptsFila1">0</label></td>
     </tr>
     <tr>
-        <td>huesos</td>
-        <td>
-            <?php echo crearImg($ruta,"cerdo.jpg" ,"fila2", 50, 1); ?>
-        </td>
-        <td>
-            <?php echo crearImg($ruta,"tigre.jpg" ,"fila2", 50, 2); ?>
-        </td>
-        <td>
-            <?php echo crearImg($ruta,"ojo.jpg" ,"fila2", 50, 3); ?>
-        </td>
-        <td>
-            <label for="" id="ptsFila2">0</label>
-        </td>
+        <td>musculos</td>
+        <td><?php echo $f21 ?></td>
+        <td><?php echo $f22 ?></td>
+        <td><?php echo $f23 ?></td>
+        <td><label for="" id="ptsFila2">0</label></td>
     </tr>
     <tr>
         <td>
@@ -74,33 +61,35 @@ $f1c = crearImg($ruta,  "ojo.jpg" ,"fila1", 50, 3);
 <!------------------------ script   ---------------------------------------->
 <script src="../libs/jquery321.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         var imgFila1Selected = 1;
 
         var fila1 = $(".fila1");
         var fila2 = $(".fila2");
 
 
-        $(".fila1").click(function(){
+        $(".fila1").click(function () {
             $("#ptsFila1").text(this.dataset.puntos);
             $('.fila1').find('*').addClass('fila11');
             //this.classList.add("fila11");
             actualizarSuma();
         });
 
-        $(".fila2").click(function(){
+        $(".fila2").click(function () {
             $("#ptsFila2").text(this.dataset.puntos);
-            $('.fila2').each(function()
-            {
+
+            // quitar selección
+            $('.fila2').each(function () {
                 $(this).removeClass("fila11");
             });
+            // poner selección
             this.classList.add("fila11");
             actualizarSuma();
         });
 
-        var actualizarSuma = function(){
-            var valFila1 = parseInt (   $("#ptsFila1").text() , 10 );
-            var valFila2  = parseInt (   $("#ptsFila2").text() , 10 );
+        var actualizarSuma = function () {
+            var valFila1 = parseInt($("#ptsFila1").text(), 10);
+            var valFila2 = parseInt($("#ptsFila2").text(), 10);
             var suma = valFila1 + valFila2;
             $("#ptsTotal").text(suma);
         }
@@ -108,9 +97,12 @@ $f1c = crearImg($ruta,  "ojo.jpg" ,"fila1", 50, 3);
 </script>
 
 <style>
-    .fila11{
+    .fila11 {
         border: 2px solid #6191db;
     }
+
+
+
 </style>
 
 

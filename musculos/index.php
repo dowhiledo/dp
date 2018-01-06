b622c19c60c4893ae2e12f1aefa92ba2feddafa3
userk <userk@email.com>
2018-01-06 15:54:09 -0500

    generarl btn img

--- musculos/index.php
+++ musculos/index.php
@@ -15,13 +15,15 @@ function crearImg($ruta, $nombre, $clase, $dim, $puntos) {
     return $tagImg;
 }
 
-$f11 = crearImg($ruta, "cerdo.jpg", "fila1", 50, 1);
-$f12 = crearImg($ruta, "tigre.jpg", "fila1", 50, 2);
-$f13 = crearImg($ruta, "ojo.jpg", "fila1", 50, 3);
+$claseF1 = "filamus fila1";
+$claseF2 = "filamus fila2";
+$f11 = crearImg($ruta, "cerdo.jpg", $claseF1, 50, 1);
+$f12 = crearImg($ruta, "tigre.jpg", $claseF1, 50, 2);
+$f13 = crearImg($ruta, "ojo.jpg"  , $claseF1, 50, 3);
 
-$f21 = crearImg($ruta, "cerdo1.jpg", "fila2", 50, 1);
-$f22 = crearImg($ruta, "tigre1.jpg", "fila2", 50, 2);
-$f23 = crearImg($ruta, "ojo1.jpg", "fila2", 50, 3);
+$f21 = crearImg($ruta, "cerdo1.jpg", $claseF2, 50, 1);
+$f22 = crearImg($ruta, "tigre1.jpg", "$claseF2", 50, 2);
+$f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, 50, 3);
 
 ?>
 
@@ -41,7 +43,7 @@ $f23 = crearImg($ruta, "ojo1.jpg", "fila2", 50, 3);
         <td><?php echo $f11 ?></td>
         <td><?php echo $f12 ?></td>
         <td><?php echo $f13 ?></td>
-        <td><label for="" id="ptsFila1">0</label></td>
+        <td><label id="ptsFila1">0</label></td>
     </tr>
     <tr>
         <td>musculos</td>
@@ -62,30 +64,21 @@ $f23 = crearImg($ruta, "ojo1.jpg", "fila2", 50, 3);
 <script src="../libs/jquery321.js"></script>
 <script>
     $(document).ready(function () {
-        var imgFila1Selected = 1;
 
-        var fila1 = $(".fila1");
-        var fila2 = $(".fila2");
+        var classImgSelected = "imgSelected";
+        $(".filamus").click(function () {
+            var resultLabel = this.parentNode.parentNode.getElementsByTagName("label")[0];
+            resultLabel.innerHTML = (this.dataset.puntos);
 
-
-        $(".fila1").click(function () {
-            $("#ptsFila1").text(this.dataset.puntos);
-            $('.fila1').find('*').addClass('fila11');
-            //this.classList.add("fila11");
+            var listImagenesFila = this.parentNode.parentNode.getElementsByTagName("img");
+            for (var i = 0; i < listImagenesFila.length; i++) {
+                var obj = listImagenesFila[i];
+                obj.classList.remove(classImgSelected);
+            }
+            this.classList.add(classImgSelected);
             actualizarSuma();
         });
-
-        $(".fila2").click(function () {
-            $("#ptsFila2").text(this.dataset.puntos);
-
-            // quitar selección
-            $('.fila2').each(function () {
-                $(this).removeClass("fila11");
-            });
-            // poner selección
-            this.classList.add("fila11");
-            actualizarSuma();
-        });
+//
 
         var actualizarSuma = function () {
             var valFila1 = parseInt($("#ptsFila1").text(), 10);
@@ -97,9 +90,17 @@ $f23 = crearImg($ruta, "ojo1.jpg", "fila2", 50, 3);
 </script>
 
 <style>
-    .fila11 {
+
+    table td .imgSelected {
         border: 2px solid #6191db;
     }
+
+    table .filamus {
+        border: 2px solid transparent;
+    }
+
+
+
 
 
 

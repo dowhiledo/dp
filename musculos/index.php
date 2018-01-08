bce53861289431c303e9f42bf5fdd3ebb930d366
userk <userk@email.com>
2018-01-07 21:05:03 -0500

    + conversion label a input:readonly no funcional

--- musculos/index.php
+++ musculos/index.php
@@ -17,18 +17,23 @@ function crearImg($ruta, $nombre, $clase, $dim, $puntos) {
 
 $claseF1 = "filamus fila1";
 $claseF2 = "filamus fila2";
-$f11 = crearImg($ruta, "cerdo.jpg", $claseF1, 50, 1);
-$f12 = crearImg($ruta, "tigre.jpg", $claseF1, 50, 2);
-$f13 = crearImg($ruta, "ojo.jpg"  , $claseF1, 50, 3);
 
-$f21 = crearImg($ruta, "cerdo1.jpg", $claseF2, 50, 1);
-$f22 = crearImg($ruta, "tigre1.jpg", "$claseF2", 50, 2);
-$f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, 50, 3);
+$medida = '160';
+$height = $medida;
+$width = $medida;
+
+$f11 = crearImg($ruta, "cerdo.jpg", $claseF1, $medida, 1);
+$f12 = crearImg($ruta, "tigre.jpg", $claseF1, $medida, 2);
+$f13 = crearImg($ruta, "ojo.jpg"  , $claseF1, $medida, 3);
+
+$f21 = crearImg($ruta, "cerdo1.jpg", $claseF2, $medida, 1);
+$f22 = crearImg($ruta, "tigre1.jpg", "$claseF2", $medida, 2);
+$f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, $medida, 3);
 
 ?>
 
 
-<table class="table tabla1mus">
+<table class="table tabla1mus col-6">
     <thead>
     <tr>
         <th>Row</th>
@@ -62,6 +67,7 @@ $f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, 50, 3);
 </table>
 <!------------------------ script   ---------------------------------------->
 <script src="../libs/jquery321.js"></script>
+<script src="../libs/bootstrap-3.3.7-dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
 
@@ -70,11 +76,10 @@ $f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, 50, 3);
             var resultLabel = this.parentNode.parentNode.getElementsByTagName("label")[0];
             resultLabel.innerHTML = (this.dataset.puntos);
 
-            var listImagenesFila = this.parentNode.parentNode.getElementsByTagName("img");
-            for (var i = 0; i < listImagenesFila.length; i++) {
-                var obj = listImagenesFila[i];
-                obj.classList.remove(classImgSelected);
-            }
+            var listaImagenesFila = this.parentNode.parentNode.getElementsByTagName("img");
+//            listaQuitarClase(listaImagenesFila,classImgSelected);
+            listaImagenesFila = Array.from(listaImagenesFila);
+            listaImagenesFila.map(img=>img.classList.remove(classImgSelected));
             this.classList.add(classImgSelected);
             actualizarSuma();
         });
@@ -86,24 +91,49 @@ $f23 = crearImg($ruta, "ojo1.jpg"  , $claseF2, 50, 3);
             var suma = valFila1 + valFila2;
             $("#ptsTotal").text(suma);
         }
+
+        var listaQuitarClase = function(lista,clase){
+            for (var i = 0; i < lista.length; i++) {
+                var obj = lista[i];
+                obj.classList.remove(clase);
+            }
+        }
+
     });
 </script>
 
 <style>
 
-    table td .imgSelected {
-        border: 2px solid #6191db;
+
+    .filamus {
+        border: .3em solid #eae7ff;
+        cursor: pointer;
     }
 
-    table .filamus {
-        border: 2px solid transparent;
+    .imgSelected {
+        border: .3em solid #6191db;
     }
-
 
 
 
 
 
 </style>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../libs/bootstrap-3.3.7-dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
+
+
+
+
+
+
+
+
+
 
 
